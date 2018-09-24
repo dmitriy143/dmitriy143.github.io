b19d4564b35b20bdc5ffc155c3e91e35068b5bc8
dmitriy143 <dima1986_09@list.ru>
2018-09-24 21:42:31 +0300

    commit 3

---
+++ nerds/js/script-catalog.js
@@ -0,0 +1,55 @@
+var link = document.querySelector(".button-footer");
+var popup = document.querySelector(".modal");
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=text]");
+var email = popup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+  var storage = "";
+  
+  try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+
+	if (storage) {
+      login.value = storage;
+      password.focus();
+    } else {
+      login.focus();
+    }
+});
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
+  popup.classList.remove("modal-error-toggle");
+});
+
+
+form.addEventListener("submit", function (evt) {
+   if (!login.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+    popup.classList.toggle("modal-error-toggle");
+  } else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", login.value);
+      }
+  }
+});
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }
+  });
